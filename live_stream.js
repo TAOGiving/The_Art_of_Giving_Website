9fc6d6c02f22613a93a1710be0930c5a6f9447d1
Alex Williams <axelwill@live.com>
2026-03-01 23:01:37 +0000

    live stream js updated not tested as yet

---
+++ live_stream.js
@@ -0,0 +1,18 @@
+async function checkLiveStatus() {
+  const apiKey = "REMOVED";
+  const channelId = "UCy3pUVDkt495v8gv5vB0jUw";
+
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+
+  if (data.items && data.items.length > 0) {
+    document.getElementById("live-indicator").style.display = "block";
+  }
+}
+
+checkLiveStatus();
+setInterval(checkLiveStatus, 60000); // check every 60 seconds
